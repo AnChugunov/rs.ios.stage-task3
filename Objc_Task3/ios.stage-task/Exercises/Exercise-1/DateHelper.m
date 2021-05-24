d4156a619b0b2ff99e4ef344325dea70d342b4ec
AnChugunov <justtobehappy123@gmail.com>
2021-05-24 21:54:11 +0300

    done 2 excuse

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,53 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    if (monthNumber < 0 || monthNumber > 12) {
+        return nil;
+    }
+    else {
+        NSDateFormatter *dateFormatter = [NSDateFormatter new];
+        NSString *month = [[dateFormatter monthSymbols] objectAtIndex:monthNumber - 1];
+        return month;
+    }
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateFormatter *dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDate *enterDay = [dateFormatter dateFromString:date];
+    if (date == nil) {
+            return 0;
+        }
+    long currentDay = [calendar component:NSCalendarUnitDay fromDate:enterDay];
+    return currentDay;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *dateFormatter =  [NSDateFormatter new];
+    dateFormatter.locale =[[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    dateFormatter.dateFormat = @"E";
+    return [dateFormatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *currentDateComponents = [calendar components: NSCalendarUnitWeekOfYear fromDate:[NSDate date] ];
+    NSUInteger  currentWeek = [currentDateComponents weekOfYear];
+    NSDateComponents *inputDateComponents = [calendar components: NSCalendarUnitWeekOfYear fromDate: date ];
+    NSUInteger inputWeek = [inputDateComponents weekOfYear];
+    if (currentWeek == inputWeek) {
+        return YES;
+    } else {
+        return NO;
+    }
+    
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,20 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    if (ratingArray.count < 3 ||  ratingArray == nil) {
+        return 0;
+    }
+    NSInteger counter = 0;
+    for (int i = 0; i < ratingArray.count - 2; i++) {
+        for (int j = i + 1; j< ratingArray.count -1; j++) {
+            for (int k = j + 1; k < ratingArray.count; k++) {
+                if ((ratingArray[i].doubleValue < ratingArray[j].doubleValue) == (ratingArray[j].doubleValue < ratingArray[k].doubleValue)) {
+                    counter++;
+                }
+            }
+        }
+    }
+    return counter;
 }
 
 @end
