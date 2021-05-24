098ddf4ce15aaf45b7bf1c2d3d6feb5454b17f50
AnChugunov <justtobehappy123@gmail.com>
2021-05-24 23:09:18 +0300

    all excs done

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,20 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *str1 = [NSMutableString stringWithString: string1];
+    NSMutableString *str2 = [NSMutableString stringWithString: string2];
+    NSMutableString *res = [NSMutableString new];
+    while ((str1.length != 0) && (str2.length != 0)) {
+        if ([str2 characterAtIndex:0] >= [str1 characterAtIndex:0]) {
+            [res appendFormat:@"%c",[str1 characterAtIndex:0]];
+            [str1 deleteCharactersInRange:NSMakeRange(0, 1) ];
+        } else {
+            [res appendFormat:@"%c",[str2 characterAtIndex:0]];
+            [str2 deleteCharactersInRange:NSMakeRange(0, 1)];
+        }
+    }
+    str1.length == 0 ? [res appendString:str2] : [res appendString:str1];
+    return res;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-4/LevelOrderTraversal.m
@@ -1,5 +1,32 @@
 #import "LevelOrderTraversal.h"
 
+NSUInteger Index(NSArray *tree, NSMutableArray *res, NSUInteger start, NSUInteger distance) {
+    NSUInteger index = start;
+    for (int directionIndex = 0; directionIndex <= 1 && index + 1 <= tree.count; directionIndex++) {
+        id nodeValue = tree[index++];
+        if (![nodeValue isKindOfClass:NSNull.class]) {
+            if (res.count <= distance) {
+                NSMutableArray *newArray = [NSMutableArray new];
+                [res addObject:newArray];
+            }
+            [res[distance] addObject:nodeValue];
+            index = Index(tree, res, index, distance + 1);
+        }
+    }
+    return index;
+}
 NSArray *LevelOrderTraversalForTree(NSArray *tree) {
-    return nil;
+    NSMutableArray *res = [NSMutableArray new];
+    
+    if (tree.count == 0 || nil == tree) {
+        return res;
+    }
+    else if (tree.count == 1) {
+        if (![tree[0] isKindOfClass:NSNull.class]) {
+            [res addObject:@[tree[0]]];
+        }
+        return res;
+    }
+    Index(tree, res, 0, 0);
+    return res;
 }
